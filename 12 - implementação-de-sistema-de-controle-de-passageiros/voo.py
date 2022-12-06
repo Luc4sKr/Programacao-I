a1e12ccfbcd4f1ebebd1dd01221197fb2c23dd11
Lucas <lucaskreuch77@gmail.com>
2022-12-06 16:48:36 -0300

    Sistema de controle de passageiros

--- 12 - implementação-de-sistema-de-controle-de-passageiros/voo.py
+++ 12 - implementação-de-sistema-de-controle-de-passageiros/voo.py
@@ -6,6 +6,9 @@ class Voo:
         self.data = data
         self.max_passageiros = 100
 
+        self.ASSENTO_DISPONIVEL = True
+        self.ASSENTO_VAGO = False
+
         self.lista_assentos = list()
         self.carga_assentos()
 
@@ -16,17 +19,18 @@ class Voo:
                     return f"Assento {i+1} livre"
 
     def verifica(self, num_assento): # Verifica se o número da cadeira recebido como parâmetro está ocupada
-        return self.lista_assentos[num_assento-1]
+        if self.lista_assentos[num_assento-1]:
+            return "Não está ocupada"
+        return "Está ocupada"
 
     def ocupa(self, num_assento): # Ocupa determinda cadeira do voô
         if self.lista_assentos[num_assento-1]:
-            self.lista_assentos[num_assento-1] = False
+            self.lista_assentos[num_assento-1] = self.ASSENTO_VAGO
             return "Operação bem sucedida"
-        else:
-            return "Operação mal sucedida"
+        return "Operação mal sucedida"
 
     def vagas(self):
-        return self.lista_assentos.count(True)
+        return self.lista_assentos.count(self.ASSENTO_DISPONIVEL)
 
     def getVoo(self): # Retorna o número do voô
         return self.num_voo
@@ -35,8 +39,12 @@ class Voo:
         return self.data
 
     def carga_assentos(self):
+        # Todas os assentos disponíveis são um valor True na lista
+        # Os assentos não disponíveis são False 
         for assento in range(1, self.max_passageiros+1):
-            self.lista_assentos.append(True)
+            self.lista_assentos.append(self.ASSENTO_DISPONIVEL)
+
+
 
 class Voo_Mod(Voo):
     def __init__(self, num_voo, data, num_assentos, assento_fumantes) -> None:
@@ -54,11 +62,9 @@ class Voo_Mod(Voo):
     def tipo(self, num_assento):
         if num_assento >= self.num_assentos - self.assento_fumantes:
             return "F"
-        else:
-            return "N"
+        return "N"
 
 
 voo = Voo(1, Data("21 December, 2022"))
 vooMod = Voo_Mod(2, Data("21 December, 2022"), 100, 20)
 
-print(vooMod.tipo(80))
